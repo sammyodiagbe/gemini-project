c87486316f61e23c6442d07f8273a697ca4a5fb6
Samson Odiagbe <sammiodiagbe@gmail.com>
2024-08-12 14:55:33 -0700

    bug: fixing bugs

--- src/app/pdf/layout.tsx
+++ src/app/pdf/layout.tsx
@@ -1,13 +1,9 @@
-import ImagesComponent from "@/components/chat/imagesComponent";
 import Navbar from "@/components/navbar";
 import NotesComponent from "@/components/notes-components/notesComponent";
-import PopupComponent from "@/components/popupComponent";
-import ToastWrapperProvider from "@/components/toast-components/toastWrapperProvider";
 import ConversationContextProvider from "@/context/conversationContext";
 import NoteContextProvider from "@/context/noteContext";
 import PopupContextProvider from "@/context/popupContext";
 import QuizContextProvider from "@/context/quizContext";
-import ToastProvider from "@/context/toastContext";
 import { FC } from "react";
 
 type LayoutType = {
